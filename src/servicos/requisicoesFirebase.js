803cd7f0594fc25fe0f50b9c81579e2af800fe46
HiensenLima <hiensen.lima@sienacompany.com>
2022-11-06 17:34:35 -0300

    requisições no Firebase, máscaras e tratamento de erros

--- src/servicos/requisicoesFirebase.js
+++ src/servicos/requisicoesFirebase.js
@@ -1,16 +1,36 @@
 import { auth } from '../config/firebase';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
+function errosFirebase(error){
+  let mensagem = '';
 
-export async function cadastrar(email, senha, confirmaSenha){
+  switch(error.code){
+    case AuthErrorCodes.EMAIL_EXISTS:
+      mensagem = "Esse email já está em uso";
+      break;   
+    case AuthErrorCodes.INVALID_EMAIL:
+      mensagem = "Email inválido";
+      break;    
+    case AuthErrorCodes.WEAK_PASSWORD:
+      mensagem = "A senha precisa de no mínimo 6 caracteres";
+      break; 
+    default:
+      mensagem = "Erro desconhecido";
+  }
+  return mensagem;
+}
+
+export async function cadastrar(email, senha){
   
-    createUserWithEmailAndPassword(auth, email, senha)
-  .then((dadosUsuario) => { 
-    console.log(dadosUsuario);
+  const resultado = await createUserWithEmailAndPassword(auth, email, senha)
+  .then((dadosDoUsuario) => { 
+    console.log(dadosDoUsuario);
+    return "sucesso";
   })
   .catch((error) => {
-
     console.log(error);
-    
+    return errosFirebase(error)
   });
+
+  return resultado;
 }
--- src/telas/Cadastro/index.js
+++ src/telas/Cadastro/index.js
@@ -1,22 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import estilos from './estilos';
 import { cadastrar } from '../../servicos/requisicoesFirebase';
 
-
 export default function Cadastro({ navigation }) {  
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmaSenha, setConfirmaSenha] = useState('');
-
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
+  
   async function realizarCadastro(){
-    
-  await cadastrar(email, senha, confirmaSenha)
-  setEmail('')
-  setSenha('')
-  setConfirmaSenha('')
+    if(email == '')
+    {
+      setMensagemError('Preencha com seu email');
+      setStatusError('email');
+    }
+    else if(senha == '')
+    {
+      setMensagemError('Digite sua senha');
+      setStatusError('senha');
+    }
+    else if(confirmaSenha == '')
+    {
+      setMensagemError('Confirme sua senha');
+      setStatusError('confirmaSenha');
+    }
+    else if(confirmaSenha != senha)
+    {
+      setMensagemError('As senhas não conferem');
+      setStatusError('confirmaSenha');
+    }
+    else{
+      
+      const resultado =  await cadastrar(email, senha)
+      
+      if(resultado == 'sucesso'){
+        
+        alert('Usuário cadastrado com sucesso');
+
+        setEmail('')
+        setSenha('')
+        setConfirmaSenha('')
+
+      }else{
+        alert(resultado);
+        // prompt(resultado); com o promt ele está rodando;
+      }
+      setStatusError('')
+      setMensagemError('')
+    }
  }
 
   return (
@@ -25,12 +60,16 @@ export default function Cadastro({ navigation }) {
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error ={statusError == 'email'}
+        messageError = {mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error ={statusError == 'senha'}
+        messageError = {mensagemError}
       />
 
       <EntradaTexto
@@ -38,6 +77,8 @@ export default function Cadastro({ navigation }) {
         value={confirmaSenha}
         onChangeText={texto => setConfirmaSenha(texto)}
         secureTextEntry
+        error ={statusError == 'confirmaSenha'}
+        messageError = {mensagemError}
       />
       
       <Botao onPress={() => realizarCadastro()}>CADASTRAR</Botao>
